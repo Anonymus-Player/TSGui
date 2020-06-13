e1de5bb4ec0e95efa59ccb746e3870ef51763ad4
CatalinNic1 <catalinnicolau2013@gmail.com>
2020-06-13 20:59:54 +0300

    trying again

--- TSGui.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Creators/Creators.hpp"
-
-namespace Gui
-{
-    #include <TGUI/TGUI.hpp>
-    using namespace tgui;
-}// namespace Gui 
---
+++ TSGui/TSGui.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <TGUI/TGUI.hpp>
+#include "Creators/Creators.hpp"
